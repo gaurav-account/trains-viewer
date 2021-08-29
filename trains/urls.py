cc6391c3ff347609c590b3bc09fcd55c4905a439
Gaurav Kumar L <gaurav.l.kumar@ericsson.com>
2021-08-29 20:16:36 +0530

    Improvements

--- trains/urls.py
+++ trains/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('departures/', include('departures.urls')),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
