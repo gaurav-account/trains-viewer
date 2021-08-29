af00f60f1d00973c9631bb0c9fc21e8f10d449a2
Gaurav Kumar L <gaurav.l.kumar@ericsson.com>
2021-08-29 15:12:13 +0530

    improvements

--- departures/views.py
+++ departures/views.py
@@ -3,18 +3,27 @@ from django.shortcuts import render
 
 from departures.api_client.client import OcpAPIClient
 from departures.api_client.client import StationNotFoundError
+from django.http import HttpResponse
+import json
 import logging
 
 # Get an instance of a logger
 logger = logging.getLogger('django')
 
+#This is the method to load data for home page.
+#It fetches all station information.
 def index(request):
     logger.info('Got a call from UI to get all stations, fetching required data ..')
     client = OcpAPIClient()
     all_stations = client.get_all_stations()
     context = {'all_stations': all_stations}
+    #Uncomment below lines if we need json in response of API
+    #data = json.dumps(all_stations)
+    #print(data)
+    #return HttpResponse(data, content_type='application/json')
     return render(request, 'departures/index.html', context)
 
+#Method to fetch all the departures from the selected station.
 def departures(request, station_code):
     logger.info('Got a call from UI to get all departures, fetching required data ..')
     client = OcpAPIClient()
